91faaa809ec6a2d2c83fa6b6717a199c4bad5fc9
David Silva <davidavilasilva@gmail.com>
2025-03-22 01:58:58 +0000

    These changes won't fix anything.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './core/components/nav/nav.component';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'interview-prep';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    console.log('AppComponent initialized');
+    this.authService.getApiKey().subscribe(apiKey => {
+      console.log('API Key:', apiKey);
+    });
+  }
 }
