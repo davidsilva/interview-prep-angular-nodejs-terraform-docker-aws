658444924f7d49c15e9fa3356d948cc708df9d9a
David Silva <davidavilasilva@gmail.com>
2025-03-22 01:26:42 +0000

    All this has been harder than expected. Hard to explain: we have to avoid trying to get the api key when we're getting the api key. Does that make sense? We're intercepting all http requests, and saying we need the api key before we can make the request. But the request for the api key itself doesn't need an api key.

--- frontend/src/app/core/interceptors/api-key.interceptor.ts
+++ frontend/src/app/core/interceptors/api-key.interceptor.ts
@@ -3,12 +3,18 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { switchMap, take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ApiKeyInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // Avoid insane circular dependency issues by not intercepting requests to get the API key
+    if (req.url === `${environment.apiBaseUrl}/get-api-key`) {
+      return next.handle(req);
+    }
+
     return this.authService.getApiKey().pipe(
         take(1),
         switchMap(apiKey => {
