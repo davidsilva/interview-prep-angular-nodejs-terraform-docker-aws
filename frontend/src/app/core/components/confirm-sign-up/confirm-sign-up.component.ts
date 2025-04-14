e21ad07ffb6f91f5262716a57c5afb53a16da0a9
David Silva <davidavilasilva@gmail.com>
2025-04-14 22:40:42 +0000

    Begin work on sign-in, sign-up, sign-out, and confirm-sign-up components

---
+++ frontend/src/app/core/components/confirm-sign-up/confirm-sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-sign-up.component.html',
+  styleUrl: './confirm-sign-up.component.scss'
+})
+export class ConfirmSignUpComponent {
+
+}
